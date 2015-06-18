08a8b990b4e569e23caf48011062a264c7a8a736
alice <alice.lilou@gmail.com>
2015-06-18 18:14:09 +0200

    typo

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: 
 	ocamlbuild -use-ocamlfind ppxSpec.native
-	ocamlc -o test -ppx ./ppxSpec.native testType.ml
+	ocamlc -o test -ppx ./ppxSpec.native testSpecPar.ml
 clean:
 	-ocamlbuild -clean
 	-rm -rf *.cm* test
--- ppxSpec.ml
+++ ppxSpec.ml
@@ -1,4 +1,3 @@
-(*Coucou*)
 open Ast_mapper                                                            
 open Ast_helper                                                                                      
 open Asttypes                                                                                        
@@ -6,27 +5,46 @@ open Parsetree
 open Longident                                                                                       
 open Ast_mapper                                                                                      
 
-(*
-let makeModule name loc = 
- let expr = {pmod_desc = Pmod_structure [{pstr_desc = Pstr_value (Nonrecursive, {pvb_pat = {txt="affiche";loc=loc}; pvb_expr = Exp.mk (Exp.let_ Nonrecursive [] }) ;pstr_loc = loc}]; pmod_loc=loc;pmod_attribute = []} in 
- Pstr_module {pmb_name = {txt="Spec"; loc = loc} ; pmb_expr = expre ;pmb_attributes=[] pmb_loc = loc}
-*)
+let f kind = 
+match kind with
+Ptype_variant l -> List.iter (fun e -> let s =  e.pcd_name.txt in Printf.printf "ici : %s\n" s) l
+|_ -> raise Not_found
 
-let makeModule name loc= 
+
+let makeModuleType name loc kind=
+f kind;  
 let ex = Exp.constant ~loc (Const_string (name,None)) in
- [%stri module Spec = struct
-    let affiche () = Printf.printf "%s\n" [%e ex] 
- end]
+[%stri module Spec = struct
+let affiche () = Printf.printf "%s\n" [%e ex] 
+end]
+
+
+let rec makeModule l = 
+ match l with
+  [] -> ()
+  |h::q -> Printf.printf "NomType : %s\n" h.ptype_name.txt; makeModule q
 
 let makeSpec argv =                                                                           
 { default_mapper with                                                                                
-  structure_item= fun mapper type_dec ->                                                                          
-    match type_dec with                                                                                  
+  structure_item= fun mapper type_desc ->                                                                          
+    match type_desc with                                                                                  
     | {  pstr_desc =
-              Pstr_type [{ptype_name; ptype_params; ptype_cstrs; ptype_kind; 
-              ptype_private;ptype_attributes=[({txt="spec";loc},p)];ptype_loc}]; pstr_loc} -> makeModule ptype_name.txt loc 
-        (* Delegate to the default mapper. *)                                                      
-        | x -> default_mapper.structure_item mapper x;                                                       
+      Pstr_type l ; pstr_loc} -> 
+      begin
+      let rec findSpec li = 
+       match li with 
+       |[] -> type_desc
+       |h::q -> if List.exists (fun (e,f) -> e.txt="spec") (h.ptype_attributes) then 
+                 begin Printf.printf "ici\n"; 
+                       makeModule l; type_desc 
+                 end
+                else 
+                 findSpec q
+       in
+        findSpec l 
+      end
+          (* Delegate to the default mapper. *)                                                      
+          | x -> default_mapper.structure_item mapper x;                                                       
 }                                                                                                    
 
 let () = register "ppxSpec" makeSpec
